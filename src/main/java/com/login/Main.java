1028b4b47dbbeacda77a84aef4ea372bf460cf04
Isalvan <78687284+Isalvan@users.noreply.github.com>
2024-05-17 02:44:58 +0200

    Add files via upload

--- src/main/java/com/login/Main.java
+++ src/main/java/com/login/Main.java
@@ -4,6 +4,7 @@
  */
 package com.login;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
@@ -12,6 +13,7 @@ import javax.swing.UIManager;
 
 /**
  * Clase principal que inicia la aplicación de inicio de sesión.
+ *
  * @author Marcos
  * @author Álvaro
  */
@@ -19,15 +21,19 @@ public class Main {
 
     /**
      * Método principal que inicia la aplicación.
-     * @param args Los argumentos de línea de comandos (no se utilizan en este caso)
+     *
+     * @param args Los argumentos de línea de comandos (no se utilizan en este
+     * caso)
      */
     public static void main(String args[]) {
 
-        FlatRobotoFont.install();
-        FlatLaf.registerCustomDefaultsSource("raven.combobox");
-        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
-        FlatMacDarkLaf.setup();
-        
+        try {
+            // Configura el tema FlatLaf (en este caso, FlatDarkLaf)
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Error al configurar FlatLaf: " + ex);
+        }
+
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
